3e7982d289363ee8a5f65fbde79d6205f32e437a
mjjuuu <gwak020910@naver.com>
2025-07-24 13:57:03 +0900

    내비게이션바(하단탭) 추가 프래그먼트 추가

--- app/src/main/java/com/example/fitroutine/MainActivity.kt
+++ app/src/main/java/com/example/fitroutine/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -18,5 +19,35 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    // 홈 탭 클릭 시
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frame, HomeFragment())
+                        .commit()
+                    true
+                }
+                R.id.nav_video -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frame, SearchFragment())
+                        .commit()
+                    true
+                }
+                R.id.nav_mypage -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frame, ProfileFragment())
+                        .commit()
+                    true
+                }
+                else -> false
+            }
+        }
+
+        // 처음에 보여줄 프래그먼트
+        bottomNavigation.selectedItemId = R.id.nav_home
     }
 }
