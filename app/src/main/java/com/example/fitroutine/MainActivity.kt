1a3e657761bec7998c8d94a21437582d0110d4df
WON <kimy21716@gmail.com>
2025-07-24 13:59:29 +0900

    마이페이지 (즐겨찾기 제외 )

--- app/src/main/java/com/example/fitroutine/MainActivity.kt
+++ app/src/main/java/com/example/fitroutine/MainActivity.kt
@@ -9,15 +9,42 @@ import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        setBottomNavigationView()
+
+        // 앱 초기 실행 시 홈화면으로 설정
+        if (savedInstanceState == null) {
+            binding.bottomNavigationView.selectedItemId = R.id.fragment_home
+        }
+    }
+
+    fun setBottomNavigationView() {
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.fragment_home -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.main_container, HomeFragment()).commit()
+                    true
+                }
+                R.id.fragment_search -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.main_container, SearchFragment()).commit()
+                    true
+                }
+                R.id.fragment_favorite -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.main_container, FavoriteFragment()).commit()
+                    true
+                }
+                R.id.fragment_settings -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.main_container, SettingsFragment()).commit()
+                    true
+                }
+                else -> false
+            }
         }
 
         val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
